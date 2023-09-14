f8a0069cea920c5dd128d3342605e72740157c07
Fjara <robuhde@gmail.com>
2023-09-14 19:39:46 -0400

    add record version to globals

--- src/globals.h
+++ src/globals.h
@@ -2,6 +2,7 @@
 
 #define COMPONENT_NAME			"foo_skipcount"
 #define COMPONENT_VERSION		"1.1.0"
+#define CURRENT_RECORD_VERSION  2
 
 // {592AAD05-29DB-4522-ACA2-AE44CE7B0BC4}
 static const GUID guid_foo_skipcount_index = { 0x592aad05, 0x29db, 0x4522,{ 0xac, 0xa2, 0xae, 0x44, 0xce, 0x7b, 0x0b, 0xc4 } };
